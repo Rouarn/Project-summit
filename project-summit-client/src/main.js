85403e244c2fbd9b560544483589af593997bec9
我与夏季 <die3881123@163.com>
2023-03-22 19:17:41 +0800

    项目初始化及各项配置更新

--- project-summit-client/src/main.js
+++ project-summit-client/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 引入 font-awesome 图标库
 import "font-awesome/css/font-awesome.css";
 
 Vue.config.productionTip = false;
